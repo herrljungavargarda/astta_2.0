2e8350b6f6c198f527ee1b10d14598a19f2ded05
Rasmus Hedin <Rasmus.hedin@herrljunga.se>
2024-05-15 10:19:49 +0200

    Removing code with no usage

--- src/main/java/se/herrljunga/astta/analyze/Analyze.java
+++
@@ -1,8 +0,0 @@
-package se.herrljunga.astta.analyze;
-
-public interface Analyze {
-    String removeSensitiveInformation(String text);
-    String getSummarize(String text);
-    String getContext(String text);
-    String getScore(String text);
-}
